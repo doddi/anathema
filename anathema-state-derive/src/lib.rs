018a0e99f66db575a477c989baccee07e78f5795
doddi <mark.dodgson@googlemail.com>
2024-09-10 20:41:44 +0100

    learning

--- anathema-state-derive/src/lib.rs
+++ anathema-state-derive/src/lib.rs
@@ -1,11 +1,12 @@
 use manyhow::{ensure, manyhow, Result};
+use quote::format_ident;
 use quote_use::quote_use as quote;
 use syn::{self, Data, DeriveInput, Fields};
 
 static STATE_IGNORE: &str = "state_ignore";
 
 #[manyhow]
-#[proc_macro_derive(State, attributes(state_ignore))]
+#[proc_macro_derive(State, attributes(state_ignore, DefaultState))]
 pub fn state_derive(input: DeriveInput) -> Result {
     let name = &input.ident;
 
@@ -27,6 +28,8 @@ pub fn state_derive(input: DeriveInput) -> Result {
         .filter_map(|f| f.ident.as_ref())
         .map(|f| (f, f.to_string()))
         .unzip();
+
+    let component_struct = format_ident!("{}Component", name);
 
     Ok(quote! {
         # use ::anathema::state::{self, Value, ValueRef, PendingValue, Path, state, Subscriber, CommonVal};
@@ -60,5 +63,42 @@ pub fn state_derive(input: DeriveInput) -> Result {
                 None
             }
         }
+
+        struct #component_struct {}
+
+        impl Component for #component_struct {
+            type State = #name;
+            type Message = ();
+        }
     })
 }
+
+// #[manyhow]
+// #[proc_macro_derive(DefaultState)]
+// pub fn default_state_derive(input: DeriveInput) -> Result {
+//     let name = &input.ident;
+//
+//     ensure!(let Data::Struct(strct) = &input.data, input, "only structs are supported");
+//
+//     ensure!(
+//         let Fields::Named(struct_fields) = &strct.fields,
+//         strct.fields,
+//         "only named fields"
+//     );
+//
+//     let message_struct = format_ident!("{}Message", name);
+//     let component_struct = format_ident!("{}Component", name);
+//
+//     Ok(quote! {
+//         # use ::anathema::state::{self, Value, ValueRef, PendingValue, Path, state, Subscriber, CommonVal};
+//         # use ::std::any::Any;
+//         struct #name {}
+//
+//         struct #message_struct{}
+//
+//         impl Component for #component_struct {
+//             type State = MyComponentState;
+//             type Message = #message_struct;
+//         }
+//     })
+// }
